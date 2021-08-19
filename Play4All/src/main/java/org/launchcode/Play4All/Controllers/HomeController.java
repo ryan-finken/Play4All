5078f536bc7dd6aa42f930d5b0ac7b5591621a75
Ryan Finken <ryanfinken338@gmail.com>
2021-08-18 19:06:22 -0500

    added thymeleaf dependency in build.gradle. created fragments.html and index.html. put a 'hello world' fragment in index, then tested to make sure it loads as expected in the broswer.

--- Play4All/src/main/java/org/launchcode/Play4All/Controllers/HomeController.java
+++ Play4All/src/main/java/org/launchcode/Play4All/Controllers/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @RequestMapping
-    @ResponseBody
     public String index() {
-        return "Hello, world!";
+        return "index";
     }
 }
