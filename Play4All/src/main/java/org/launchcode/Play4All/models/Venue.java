ebd982a936bfec615703d8521bd0ce86f20ac036
MissGrumpling <sjermel425@gmail.com>
2021-08-25 18:07:13 -0500

    boilerplate code for Event and Venue models

--- Play4All/src/main/java/org/launchcode/Play4All/models/Venue.java
+++ Play4All/src/main/java/org/launchcode/Play4All/models/Venue.java
@@ -1,5 +1,7 @@
 package org.launchcode.Play4All.models;
 
+import jdk.jfr.Event;
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
@@ -16,5 +18,37 @@ public class Venue extends AbstractEntity{
     @OneToMany
     private List<Events> events = new ArrayList<>();
 
+    public Venue(){}
 
+    public Venue(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Events> getEvents() {
+        return events;
+    }
+    public void addEvent(Events event){
+        this.events.add(event);
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
 }
