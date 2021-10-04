e5deaabbef635ae0ea5435938674d3d777814319
Ryan Finken <ryanfinken338@gmail.com>
2021-10-04 12:47:44 -0500

    first steps towards working venue page

--- Play4All/src/main/java/org/launchcode/Play4All/Controllers/VenueController.java
+++ Play4All/src/main/java/org/launchcode/Play4All/Controllers/VenueController.java
@@ -2,11 +2,15 @@ package org.launchcode.Play4All.Controllers;
 
 
 import org.launchcode.Play4All.data.VenueRepository;
+import org.launchcode.Play4All.models.Venue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("venue")
@@ -19,6 +23,14 @@ public class VenueController {
     public String displayAllVenues(Model model) {
         model.addAttribute("venues", venueRepository.findAll());
         return "venue/index";
+    }
+
+    @GetMapping("details")
+    public String displayVenue(@RequestParam Integer venueId, Model model) {
+        Optional<Venue> result = venueRepository.findById(venueId);
+        Venue venue = result.get();
+        model.addAttribute("venue", venue);
+        return "venue/details";
     }
 
 }
